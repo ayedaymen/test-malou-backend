2302e82410c7f857c756e0f0a71bc7ceab03ac9d
Aymen Ayed <aymen.ayed@esprit.tn>
2021-05-02 15:45:58 +0200

    code covrage for postController

--- src/posts/posts.controller.spec.ts
+++ src/posts/posts.controller.spec.ts
@@ -1,6 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
+import { Posts } from './_models/posts';
+import  axios from 'axios';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('PostsController', () => {
   let controller: PostsController;
@@ -9,6 +14,7 @@ describe('PostsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PostsController],
+     
     
     }).compile();
 
@@ -18,5 +24,29 @@ describe('PostsController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
-  
+
+  describe('PostsController', () => {
+    
+    beforeEach(() => {
+      postsService  = new PostsService();
+      controller = new PostsController(postsService);
+    });
+    describe('findAll', () => {
+      it('should return an array of posts', async () => {
+
+        const result = [{ id: 1,
+          name :"46aea",
+          votes_count: 152588447,
+          day:"01-05-2021" },
+
+        {  id: 1,
+          name:"46aea",
+          votes_count: 152588447,
+          day:"string" }];
+          mockedAxios.get.mockImplementationOnce(() => Promise.resolve(result));
+
+        expect(await controller.getPostByDay("2021-02-04")).toBe(result);
+      });
+    });
+  });
 });
